a502ae58c2ecb5304fe598841a91a8bd29858854
Gerson <ggarridom@uni.pe>
2018-08-28 09:41:45 -0500

    fist commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+// Para levantar el server en otro terminal correr las siguientes lineas
+// json-server --watch db.json --port 8000
+
+// Para hacer un fetch usar axios (tienes que instalarlo)
+// http://localhost:3000/users 
+// te dara todos los users
+
+// Ejercicio 1. Mostrar en una tabla los datos que se piden
+// Ejercicio 2. Filtrar para mostrar solo los que tengan total debt mayor a 2K
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
